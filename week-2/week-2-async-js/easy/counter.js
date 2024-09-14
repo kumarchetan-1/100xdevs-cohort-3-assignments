a621b2595f667b69e762f549040c14f8987e3429
Chetan <kumarchetan.npr@gmail.com>
2024-09-15 03:59:45 +0530

    Add more refined code

--- week-2/week-2-async-js/easy/counter.js
+++ week-2/week-2-async-js/easy/counter.js
@@ -1,15 +1,13 @@
 function countUp(n = 0){
-    // if (n<100) {
-    //     setTimeout(()=>{
-    //         console.log(n)
-    //         countUp(n+1)
-    //     }, 1000)
-    // }
+        setTimeout(()=>{
+            console.log(n)
+            countUp(n+1)
+        }, 1000)
 
-setInterval(()=>{
-    console.log(n)
-    n++
+// setInterval(()=>{
+//     console.log(n)
+//     n++
 
-}, 1000)
+// }, 1000)
 }
 countUp()
